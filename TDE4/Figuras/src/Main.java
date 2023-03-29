dde66f1f9aa08995a2516c447830f6806cb12336
Ramoni de Matos Bauer <112291325+Ramonibauer@users.noreply.github.com>
2023-03-29 09:29:35 -0300

    Update Main.java

--- TDE4/Figuras/src/Main.java
+++ TDE4/Figuras/src/Main.java
@@ -1,3 +1,30 @@
 public class Main {
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado();
+        quadrado.lado = 2;
+        quadrado.calulcarArea();
+        double resultado1 = quadrado.calulcarArea();
+        System.out.println(resultado1);
     
+        Trapezio trapezio = new Trapezio();
+        trapezio.altura=2;
+        trapezio.B=2;
+        trapezio.b=2;
+        trapezio.calulcarArea();
+        double resultado2 = trapezio.calulcarArea();
+        System.out.println(resultado2);
+
+        Cicunferencia cicunferencia = new Cicunferencia();
+        cicunferencia.raio = 2;
+        cicunferencia.calulcarArea();
+        double resultado3 = cicunferencia.calulcarArea();
+        System.out.println(resultado3);
+        
+        Triangulo triangulo = new Triangulo();
+        triangulo.altura = 2;
+        triangulo.base = 3;
+        triangulo.calulcarArea();
+        double resultado4 = triangulo.calulcarArea();
+        System.out.println(resultado4);
+    }
 }
